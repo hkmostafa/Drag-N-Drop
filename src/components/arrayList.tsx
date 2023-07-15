5e257f0930effcf1c23ce6753b29f8593d2a0e5b
Mostafa Hakkou <63522355+hkmostafa@users.noreply.github.com>
2023-07-15 14:58:34 +0100

    replaced sass-node with sass

--- src/components/arrayList.tsx
+++ src/components/arrayList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState,useEffect } from "react";
 import { StateContext } from "../context";
 import isDivInsideRightZone from "../utility/rightDevParent";
 
@@ -30,6 +30,9 @@ const ArrayList = () => {
     setData({ email, name, framework, technology, workStyle });
   };
 
+  useEffect(()=>{
+    showData()
+  },[isEmailInside,isNameInside,isFrameworkInside,isWorkStyleInside,istechnologyInside])
   return (
     <>
       <button
@@ -64,19 +67,19 @@ const ArrayList = () => {
             <tr className="font-PoppinsRegular text-xs sm:text-sm text-center sm:text-left   text-gray-300">
               <td className="py-2 px-1 md:px-4 border-r-[0.5px]  sm:border-none border-solid  break-words">
                 {/*Show name only if the name input is in the right dropzone */}
-                {isNameInside && data.name}
+                {isNameInside && name}
               </td>
               <td className="py-2  px-1 md:px-4 border-r-[0.5px]  sm:border-none border-solid break-words">
-                {isWorkStyleInside && data.workStyle}
+                {isWorkStyleInside && workStyle}
               </td>
               <td className="py-2  px-1 md:px-4 border-r-[0.5px]  sm:border-none border-solid  break-words">
-                {isEmailInside && data.email}
+                {isEmailInside && email}
               </td>
               <td className="py-2 px-1 md:px-4 border-r-[0.5px]  sm:border-none border-solid break-words">
-                {isFrameworkInside && data.framework}
+                {isFrameworkInside && framework}
               </td>
               <td className="py-2  px-1 md:px-4 border-solid sm:border-none  break-words ">
-                {istechnologyInside && data.technology}
+                {istechnologyInside && technology}
               </td>
             </tr>
           </tbody>
